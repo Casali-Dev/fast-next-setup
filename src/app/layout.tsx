97645ec8b52feb3545e250318acde995a7a9bf9b
Guilherme Lucca Casali <guilhermecasali.dev@gmail.com>
2024-02-26 20:56:47 +0000

    initial/create base next env for most projects

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google"
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "Fast Next Setup",
+  description: "Grab and code!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>{children}</body>
+    </html>
+  );
+}
